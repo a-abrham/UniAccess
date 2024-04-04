54b5d2e0d7ae32e00b77a87f98363af9467dad36
abrshkorra <abrshkorra@gmail.com>
2024-04-04 12:12:38 +0300

    header in home page

--- lib/header.dart
+++ lib/header.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget {
-  const Header({super.key});
+  final String university;
+
+  const Header({super.key, required this.university});
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           margin: const EdgeInsets.fromLTRB(15, 5, 0, 0),
           child: const Image(
-            image: AssetImage('asset/a.jpg'),
+            image: AssetImage('asset/bduLogo.jpg'),
             height: 60,
           ),
         ),
         Container(
-          margin: const EdgeInsets.fromLTRB(0, 20, 15, 0),
+          margin: const EdgeInsets.fromLTRB(20, 20, 0, 0),
           child: RichText(
-            text: const TextSpan(
-              text: 'Welcome ',
+            text: TextSpan(
               style: TextStyle(fontSize: 24, color: Colors.black),
               children: <TextSpan>[
                 TextSpan(
-                  text: 'Dear BDU',
+                  text: university,
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                TextSpan(text: ' Student!'),
+                TextSpan(text: ' University'),
               ],
             ),
           ),
